dae36d97ed2a3c7b9e9fe375a119f42387fced1b
datntSE <dat.nt271102@gmail.com>
2022-12-12 22:41:51 +0700

    husky

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const route = require('./routes/index.route');
 app.use(express.static(path.join(__dirname, 'public')));
 // add middleware
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -23,14 +23,13 @@ app.use(morgan('combined'));
 //template engine
 app.engine('hbs', engine({ extname: '.hbs' }));
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+            app.set('view engine', 'hbs');
+                            app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ', path.join(__dirname, 'resources/views'));
 
 // route init
-route(app);
+        route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+        app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
-
