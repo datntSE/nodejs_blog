30f746ce72ce43ffdafb6bbeff455bcfcf413f0a
datntSE <dat.nt271102@gmail.com>
2022-12-12 23:41:00 +0700

    Install prettier lint-granted husky

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(
         extended: true,
     }),
 );
-app.use(express.json());
+            app.use(express.json());
 
 //http logger
 app.use(morgan('combined'));
@@ -23,13 +23,12 @@ app.use(morgan('combined'));
 //template engine
 app.engine('hbs', engine({ extname: '.hbs' }));
 
-            app.set('view engine', 'hbs');
-                            app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'hbs');
+app.set     ('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ', path.join(__dirname, 'resources/views'));
 
 // route init
-        route(app);
-
+route(app);
         app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
